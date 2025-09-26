8aadbdf4fb4ddbbf4fa0c84b529ece9f345115f3
wuyntram04 <hothiquynhtram2004@gmail.com>
2025-09-25 21:36:26 -0700

    Add files via upload

--- input.h
+++ input.h
@@ -1,8 +1,108 @@
+#pragma once
 #include <iostream>
 #include <algorithm>
 #include <string>
 
 using namespace std;
+
+string& ltrim(string& s)
+{
+	s.erase(s.begin(), find_if_not(s.begin(), s.end(), [](int c) {return isspace(c); }));
+	return s;
+}
+
+string& rtrim(string& s)
+{
+	s.erase(find_if_not(s.rbegin(), s.rend(), [](int c) {return isspace(c); }).base(), s.end());
+	return s;
+}
+
+string trim(const string& s)
+{
+	string t = s;
+	return ltrim(rtrim(t));
+}
+
+//return an input string
+string inputString(string prompt, bool haveSpaces, int length)
+{
+	string input;
+	while (true)
+	{
+		cout << prompt;
+		if (haveSpaces)
+			getline(cin, input);
+		else
+		{
+			cin >> input;
+			cin.clear();
+			cin.ignore(999, '\n');
+		}
+
+		if (input.length() > length)
+			cout << "ERROR-2: Invalid input. Length must not be exceeded " << length << " characters.\n";
+		else
+			break;
+	}
+	input.resize(length);
+	return trim(input);
+}
+
+//return an input string
+string inputString(string prompt, bool haveSpaces)
+{
+	string input;
+	cout << prompt;
+	if (haveSpaces)
+		getline(cin, input);
+	else
+	{
+		cin >> input;
+		cin.clear();
+		cin.ignore(999, '\n');
+	}
+
+	return trim(input);
+}
+
+//return an input char
+char inputChar(string prompt, string listChars)
+{
+	char input;
+	while (true)
+	{
+		cout << prompt;
+		if (!(cin >> input))
+			cout << "ERROR-1: Invalid input. Must be a character type.\n";
+		else
+		{
+			bool bfound = false;
+			for (unsigned c = 0; c < listChars.length(); c++)
+				if (toupper(listChars[c]) == toupper(input))
+				{
+					bfound = true;
+					break;
+				}
+			if (!bfound)
+			{
+				cout << "ERROR-2: Invalid input. Must be a character from the list of '";
+				for (unsigned c = 0; c < listChars.length() - 1; c++)
+					cout << "'" << static_cast<char>(toupper(listChars[c])) << "', ";
+				cout << "or '" << static_cast<char>(toupper(listChars.back())) << "'.\n";
+			}
+			else
+				break;
+		}
+		cin.clear();
+		cin.ignore(999, '\n');
+
+	};
+
+	cin.clear();
+	cin.ignore(999, '\n');
+
+	return input;
+}
 
 //return an input char
 char inputChar(string prompt, char yes, char no)
@@ -21,38 +121,6 @@ char inputChar(string prompt, char yes, char no)
 			cout << "ERROR-2A: Invalid input. Must be a '" << static_cast<char>(toupper(yes)) << "' or '" << static_cast<char>(toupper(no)) << "' character.\n";
 		else
 			break;
-	} while (true);
-	cin.clear();
-	cin.ignore(999, '\n');
-	return input;
-}
-
-char inputChar(string prompt, string lookup)
-{
-	char input;
-	do
-	{
-		cout << prompt;
-		if (!(cin >> input))
-		{
-			cout << "ERROR-1A: Invalid input. Must be a character type.\n";
-			cin.clear();
-			cin.ignore(999, '\n');
-		}
-		else
-		{
-			for ( int i = 0; i < lookup.length(); i++)
-				if (toupper(input) == toupper(lookup[i]))
-				{
-					cin.clear();
-					cin.ignore(999, '\n');
-					return input;
-				}
-			cout << "ERROR-1A: Invalid input. Must be \"" + lookup +"\".\n";
-			cin.clear();
-			cin.ignore(999, '\n');
-		}
-		break;
 	} while (true);
 	cin.clear();
 	cin.ignore(999, '\n');
@@ -121,6 +189,8 @@ int inputInteger(string prompt)
 		else
 			break;
 	} while (true);
+	cin.clear();
+	cin.ignore(999, '\n');
 	return input;
 }
 
@@ -137,36 +207,15 @@ int inputInteger(string prompt, bool posNeg)
 			cin.clear();
 			cin.ignore(999, '\n');
 		}
-		else if (posNeg && input <= 0)
+		else if (posNeg && input < 0)
 			cout << "ERROR-2A: Invalid input. Must be a positive number.\n";
 		else if (!posNeg && input >= 0)
 			cout << "ERROR-2A: Invalid input. Must be a negative number.\n";
 		else
 			break;
 	} while (true);
-	return input;
-}
-
-//return an integer where posNeg is positive (true) or negative (false)
-int inputInteger(string prompt, int start, bool greater)
-{
-	int input;
-	do
-	{
-		cout << prompt;
-		if (!(cin >> input))
-		{
-			cout << "ERROR-2A: Invalid input. Must be an integer type.\n";
-			cin.clear();
-			cin.ignore(999, '\n');
-		}
-		else if (greater && input < start)
-			cout << "ERROR-2A: Invalid input. Must be a greater than or equal to " << start << ".\n";
-		else if (!greater && input > 0)
-			cout << "ERROR-2A: Invalid input. Must be a lesser than or equal to " << start << ".\n";
-		else
-			break;
-	} while (true);
+	cin.clear();
+	cin.ignore(999, '\n');
 	return input;
 }
 
@@ -189,6 +238,8 @@ int inputInteger(string prompt, int startRange, int endRange)
 		else
 			break;
 	} while (true);
+	cin.clear();
+	cin.ignore(999, '\n');
 	return input;
 }
 
@@ -224,32 +275,10 @@ double inputDouble(string prompt, bool posNeg)
 			cin.clear();
 			cin.ignore(999, '\n');
 		}
-		else if (posNeg && input <= 0.0)
+		else if (posNeg && input < 0.0)
 			cout << "ERROR-2B: Invalid input. Must be a positive number.\n";
 		else if (!posNeg && input >= 0.0)
 			cout << "ERROR-2B: Invalid input. Must be a negative number.\n";
-		else
-			break;
-	} while (true);
-	return input;
-}
-
-double inputDouble(string prompt, double start, bool posNeg)
-{
-	double input;
-	do
-	{
-		cout << prompt;
-		if (!(cin >> input))
-		{
-			cout << "ERROR-2B: Invalid input. Must be a double type.\n";
-			cin.clear();
-			cin.ignore(999, '\n');
-		}
-		else if (posNeg && input <= start)
-			cout << "ERROR-2B: Invalid input. Must be greater than or equal to " << start << ".\n";
-		else if (!posNeg && input >= start)
-			cout << "ERROR-2B: Invalid input. Must be lesser than or equal to " << start << ".\n";
 		else
 			break;
 	} while (true);
@@ -276,3 +305,5 @@ double inputDouble(string prompt, double startRange, double endRange)
 	} while (true);
 	return input;
 }
+
+
